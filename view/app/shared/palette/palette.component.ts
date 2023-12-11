854f3c080c1e5d6caa8e4bdd132da3db67a11108
Cartago <cartago.relaxingcup@gmail.com>
2023-12-11 21:17:45 +0100

    Comenzamos a probar para estilar labels en los inputs

--- view/app/shared/palette/palette.component.ts
+++ view/app/shared/palette/palette.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, TitleCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   templateUrl: './palette.component.html',
   styleUrls: ['./palette.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TitleCasePipe],
 })
 export class PaletteComponent {
   private _scaleSuffix = ['lightest', 'light', 'base', 'dark', 'darkest'];
